bfeb4feab8aaaa9daa53b6a14fafad26b1b4bc67
Jhusseth <jhusseth.sanchez@u.icesi.edu.co>
2021-06-10 11:02:22 -0500

    first commit

---
+++ precache-manifest.99e81fc673cffb2aa0abf6009ef0b881.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b984616845a944e13c9ee48b6b4cdbb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4371cbcc4945c86f061a",
+    "url": "/static/css/main.eb4b91a1.chunk.css"
+  },
+  {
+    "revision": "5bc99d041259b2cd706e",
+    "url": "/static/js/0.36e92dc0.chunk.js"
+  },
+  {
+    "revision": "041647f4284fc7c5033c",
+    "url": "/static/js/10.a30fc42d.chunk.js"
+  },
+  {
+    "revision": "4cb48f52305dad1dca8d",
+    "url": "/static/js/3.217dbe8e.chunk.js"
+  },
+  {
+    "revision": "d6b29286b0bbae6e17db84419d44fb6a",
+    "url": "/static/js/3.217dbe8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8f3e5e2db2db2ad9f0c",
+    "url": "/static/js/4.5dc290de.chunk.js"
+  },
+  {
+    "revision": "10e620cf7752b1bf5fc6",
+    "url": "/static/js/5.462959ea.chunk.js"
+  },
+  {
+    "revision": "c9dccffe7ed4cb834068",
+    "url": "/static/js/6.8d99e191.chunk.js"
+  },
+  {
+    "revision": "9f57e05a7a4de5849acd",
+    "url": "/static/js/7.a33f40d0.chunk.js"
+  },
+  {
+    "revision": "d6dc27a95165ee229470",
+    "url": "/static/js/8.e97bfd3c.chunk.js"
+  },
+  {
+    "revision": "672d906024a81adde1fc",
+    "url": "/static/js/9.f5f14c01.chunk.js"
+  },
+  {
+    "revision": "4371cbcc4945c86f061a",
+    "url": "/static/js/main.843fd1b3.chunk.js"
+  },
+  {
+    "revision": "1b6f6d94be9a7815fb58",
+    "url": "/static/js/runtime-main.02cd6fd9.js"
+  },
+  {
+    "revision": "c68a8c71951a0944ee9a1d3c8708932c",
+    "url": "/static/media/logo-santex.c68a8c71.png"
+  }
+]);
